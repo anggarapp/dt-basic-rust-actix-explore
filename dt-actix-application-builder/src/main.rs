b2eb9971f9d985860a2a77ba26d485508f0c8f0d
Anggara Permana Putra <anggaraperpe@gmail.com>
2024-03-23 16:31:46 +0700

    latest: Application Builder Part 2 - shared data using Mutex

--- dt-actix-application-builder/src/main.rs
+++ dt-actix-application-builder/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, web, App, HttpServer};
+use actix_web::{web, App, HttpServer};
+use std::sync::{Mutex, MutexGuard};
 
 // Start of web::scope
 // async fn index() -> impl Responder {
@@ -6,16 +7,28 @@ use actix_web::{get, web, App, HttpServer};
 // }
 // End of web::scope
 // Start of web::Data
-struct AppState {
-    app_name: String,
+// struct AppState {
+//     app_name: String,
+// }
+
+// #[get("/")]
+// async fn index(data: web::Data<AppState>) -> String {
+//     let app_name: &String = &data.app_name;
+//     format!("Hello {app_name}!")
+// }
+// End of web::Data
+// Start of web::Data
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
 }
 
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name: &String = &data.app_name;
-    format!("Hello {app_name}!")
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter: MutexGuard<i32> = data.counter.lock().unwrap();
+    *counter += 1;
+    format!("Request Number is {counter}!")
 }
 // End of web::Data
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Start of web::scope
@@ -26,13 +39,27 @@ async fn main() -> std::io::Result<()> {
     // .run()
     // .await
     // End of web::scope
+
     // Start of web::Data
-    HttpServer::new(|| {
+    // HttpServer::new(|| {
+    //     App::new()
+    //         .app_data(web::Data::new(AppState {
+    //             app_name: String::from("Ashborn"),
+    //         }))
+    //         .service(index)
+    // })
+    // .bind(("127.0.0.1", 8099))?
+    // .run()
+    // .await
+    // End of web::Data
+    // Start of web::Data
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+    HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("Ashborn"),
-            }))
-            .service(index)
+            .app_data(counter.clone())
+            .route("/", web::get().to(index))
     })
     .bind(("127.0.0.1", 8099))?
     .run()
