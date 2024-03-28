a6916cfdd87449c2cb2f72fd50a540fbe75d9d41
Anggara Permana Putra <anggaraperpe@gmail.com>
2024-03-29 04:26:08 +0700

    latest: using extractor

--- dt_actix_extractor/src/main.rs
+++ dt_actix_extractor/src/main.rs
@@ -1,8 +1,4 @@
-use actix_web::{
-    get,
-    web::{self, Json},
-    App, HttpRequest, HttpServer, Responder, Result,
-};
+use actix_web::{get, web, App, HttpRequest, HttpServer, Result};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -11,19 +7,36 @@ struct Info {
     username: String,
 }
 
-async fn index(
-    json: web::Json<MyInfo>,
-    patt: web::Path<(String, String)>,
-    evt: web::Json<Event>,
-) -> impl Responder {
-    let path = path.into_inner();
-    format!("{} {} {} {}", path.0, path.1, json.id, json.username)
+#[get("/users/v1/{user_id}/{username}")]
+async fn index_one(info: web::Path<Info>) -> Result<String> {
+    Ok(format!(
+        "Elcome {}, user_id {}",
+        info.username, info.user_id
+    ))
+}
+
+#[get("/users/v2/{user_id}/{username}")]
+async fn index_two(path: web::Path<(u32, String)>) -> Result<String> {
+    let (username, user_id) = path.into_inner();
+    Ok(format!("Elcome {}, user_id {}", username, user_id))
+}
+
+#[get("/users/v3/{user_id}/{username}")]
+async fn index_three(req: HttpRequest) -> Result<String> {
+    let username: String = req.match_info().get("username").unwrap().parse().unwrap();
+    let user_id: u32 = req.match_info().query("user_id").parse().unwrap();
+    Ok(format!("Elcome {}, user_id {}", username, user_id))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(factory)
-        .bind(("127.0.0.1", 8099))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index_one)
+            .service(index_two)
+            .service(index_three)
+    })
+    .bind(("127.0.0.1", 8099))?
+    .run()
+    .await
 }
