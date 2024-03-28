5e8857184a6a67e1bbc652bb87e05bed73c4de89
Anggara Permana Putra <anggaraperpe@gmail.com>
2024-03-29 06:51:48 +0700

    latest: handler - using streaming response

--- dt_actix_handler/src/main.rs
+++ dt_actix_handler/src/main.rs
@@ -1,33 +1,54 @@
-use actix_web::{
-    body::BoxBody, get, http::header::ContentType, App, HttpRequest, HttpResponse, HttpServer,
-    Responder,
-};
-use serde::Serialize;
+// start custom response
+// use actix_web::{
+//     body::BoxBody, get, http::header::ContentType, App, HttpRequest, HttpResponse, HttpServer,
+//     Responder,
+// };
+// use serde::Serialize;
 
-#[derive(Serialize)]
-struct MyCustomReturn {
-    name: &'static str,
-}
+// #[derive(Serialize)]
+// struct MyCustomReturn {
+//     name: &'static str,
+// }
 
-impl Responder for MyCustomReturn {
-    type Body = BoxBody;
+// impl Responder for MyCustomReturn {
+//     type Body = BoxBody;
 
-    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
-        let body = serde_json::to_string(&self).unwrap();
-        HttpResponse::Ok()
-            .content_type(ContentType::json())
-            .body(body)
-    }
-}
+//     fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+//         let body = serde_json::to_string(&self).unwrap();
+//         HttpResponse::Ok()
+//             .content_type(ContentType::json())
+//             .body(body)
+//     }
+// }
 
-#[get("/")]
-async fn index() -> impl Responder {
-    MyCustomReturn { name: "Corpencius" }
+// #[get("/")]
+// async fn index() -> impl Responder {
+//     MyCustomReturn { name: "Corpencius" }
+// }
+
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| App::new().service(index))
+//         .bind(("127.0.0.1", 8099))?
+//         .run()
+//         .await
+// }
+// end custom Response
+
+use actix_web::{get, web, App, Error, HttpResponse, HttpServer};
+use futures::{future::ok, stream::once};
+
+#[get("/stream")]
+async fn stream() -> HttpResponse {
+    let body = once(ok::<_, Error>(web::Bytes::from_static(b"arise")));
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .streaming(body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(stream))
         .bind(("127.0.0.1", 8099))?
         .run()
         .await
