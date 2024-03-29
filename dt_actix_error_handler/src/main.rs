1d3257f3ce15d560ecc943e8f5816417dbcf289b
Anggara Permana Putra <anggaraperpe@gmail.com>
2024-03-29 13:29:04 +0700

    latest: error handler with split logic

--- dt_actix_error_handler/src/main.rs
+++ dt_actix_error_handler/src/main.rs
@@ -76,22 +76,83 @@
 
 // start error handler using helper function
 
-use actix_web::{error, web, App, HttpServer, Result};
-struct MyError {
-    name: &'static str,
+// use actix_web::{error, web, App, HttpServer, Result};
+// struct MyError {
+//     name: &'static str,
+// }
+
+// async fn index() -> Result<&'static str> {
+//     let result: Result<&'static str, MyError> = Err(MyError { name: "Longinus" });
+//     Ok(result.map_err(|e| error::ErrorBadRequest(e.name))?)
+// }
+
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| App::new().service(web::resource("/").to(index)))
+//         .bind(("127.0.0.1", 8099))?
+//         .run()
+//         .await
+// }
+
+// end error handler using helper function
+
+// start error handler with split logic
+use actix_web::{
+    error,
+    http::{header::ContentType, StatusCode},
+    post, App, HttpResponse, HttpServer, Result,
+};
+use derive_more::{Display, Error};
+use serde;
+
+#[derive(Debug, Display, Error)]
+enum UserError {
+    #[display(fmt = "Internal error")]
+    InternalError,
+
+    #[display(fmt = "Invalid Input")]
+    InvalidInput,
 }
 
-async fn index() -> Result<&'static str> {
-    let result: Result<&'static str, MyError> = Err(MyError { name: "Longinus" });
-    Ok(result.map_err(|e| error::ErrorBadRequest(e.name))?)
+impl error::ResponseError for UserError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::html())
+            .body(self.to_string())
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            UserError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
+            UserError::InvalidInput => StatusCode::BAD_REQUEST,
+        }
+    }
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct MyData {
+    value: String,
+}
+
+fn just_fails() -> Result<(), UserError> {
+    Err(UserError::InternalError)
+}
+
+#[post("/proc")]
+async fn proc(data: actix_web::web::Json<MyData>) -> Result<&'static str, UserError> {
+    if data.value.is_empty() {
+        return Err(UserError::InvalidInput);
+    }
+
+    just_fails().map_err(|_e| UserError::InternalError)?;
+    Ok("Success")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(web::resource("/").to(index)))
+    HttpServer::new(|| App::new().service(proc))
         .bind(("127.0.0.1", 8099))?
         .run()
         .await
 }
-
-// end error handler using helper function
+// end error handler with split logic
