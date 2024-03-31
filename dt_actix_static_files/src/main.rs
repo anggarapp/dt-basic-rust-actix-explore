f0223f4179d473a46ea8bb9b82eb688bae95d81f
Anggara Permana Putra <anggaraperpe@gmail.com>
2024-03-31 18:00:42 +0700

    latest: simple directory listing

--- dt_actix_static_files/src/main.rs
+++ dt_actix_static_files/src/main.rs
@@ -21,52 +21,82 @@
 
 // start serve individual file with regex and sanitized path
 // use actix_files as fs; //for directory listing
-use actix_files::NamedFile;
-use actix_web::{error, web, App, HttpRequest, HttpServer, Result};
-use regex::Regex;
-use std::io;
-use std::path::{Path, PathBuf};
+// use actix_files::NamedFile;
+// use actix_web::{error, web, App, HttpRequest, HttpServer, Result};
+// use regex::Regex;
+// use std::io;
+// use std::path::{Path, PathBuf};
 
-async fn index(req: HttpRequest) -> Result<NamedFile> {
-    let filename: PathBuf = req.match_info().query("filename").parse().unwrap();
+// async fn index(req: HttpRequest) -> Result<NamedFile> {
+//     let filename: PathBuf = req.match_info().query("filename").parse().unwrap();
 
-    let filename_str = filename.to_string_lossy();
-    let filename_regex = Regex::new(r"^[\w\./_]+$").unwrap();
+//     let filename_str = filename.to_string_lossy();
+//     let filename_regex = Regex::new(r"^[\w\./_]+$").unwrap();
 
-    if !filename_regex.is_match(&filename_str) {
-        return Err(error::ErrorNotFound("invalid filename"));
-    }
+//     if !filename_regex.is_match(&filename_str) {
+//         return Err(error::ErrorNotFound("invalid filename"));
+//     }
 
-    let sanitized_filename = sanitize_filename::sanitize(&filename_str);
+//     let sanitized_filename = sanitize_filename::sanitize(&filename_str);
 
-    let base_dir = "./static";
-    let path = Path::new(&base_dir).join(&sanitized_filename);
+//     let base_dir = "./static";
+//     let path = Path::new(&base_dir).join(&sanitized_filename);
 
-    if !path.starts_with(&base_dir) {
-        return Err(error::ErrorNotFound("invalid file path"));
-    }
+//     if !path.starts_with(&base_dir) {
+//         return Err(error::ErrorNotFound("invalid file path"));
+//     }
 
-    match NamedFile::open(path) {
-        Ok(file) => Ok(file),
-        Err(err) => {
-            if err.kind() == io::ErrorKind::NotFound {
-                Err(error::ErrorNotFound("file not found"))
-            } else {
-                Err(error::ErrorInternalServerError("internal server error"))
-            }
-        }
-    }
-}
+//     match NamedFile::open(path) {
+//         Ok(file) => Ok(file),
+//         Err(err) => {
+//             if err.kind() == io::ErrorKind::NotFound {
+//                 Err(error::ErrorNotFound("file not found"))
+//             } else {
+//                 Err(error::ErrorInternalServerError("internal server error"))
+//             }
+//         }
+//     }
+// }
+
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new().route("/{filename:.*}", web::get().to(index))
+//         // .service(fs::Files::new("/static", ".").show_files_listing()) // for directory listing
+//     })
+//     .bind(("127.0.0.1", 8099))?
+//     .run()
+//     .await
+// }
+
+// end serve individual file with regex and sanitized path
+
+// start serve directory listing
+
+use actix_files as fs;
+use actix_web::{middleware, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().route("/{filename:.*}", web::get().to(index))
-        // .service(fs::Files::new("/static", ".").show_files_listing()) // for directory listing
+        App::new()
+            .wrap(middleware::DefaultHeaders::new().add(("X-Content-Type-Options", "nosniff")))
+            .service(
+                fs::Files::new("/static", ".")
+                    .show_files_listing()
+                    .use_last_modified(true)
+                    .disable_content_disposition(),
+            )
+            .default_service(
+                fs::Files::new("/static", ".")
+                    .index_file("index.html")
+                    .use_last_modified(true)
+                    .disable_content_disposition(),
+            )
     })
-    .bind(("127.0.0.1", 8099))?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
 
-// end serve individual file with regex and sanitized path
+// end serve directory listing
